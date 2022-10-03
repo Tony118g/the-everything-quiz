17cf2069291b98071e421c7af59e7b954a91005a
Tony118g <guimaraesantony@gmail.com>
2022-10-03 09:22:38 +0000

    Add function to display difficulty selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,12 +33,37 @@ function displayRules() {
     closeRulesButton.addEventListener("click", closeRules);
 }
 
+/**
+ * Hides the rules modal and displays the main menu.
+ */
+
 function closeRules() {
     rulesModal.classList.add("hide");
     heading.classList.remove("hide");
     menu.classList.remove("hide");
 }
 
+/**
+ * Displays the difficulty selection while hiding the main menu 
+ * as well as gets the difficulty selection buttons 
+ * and adds event listeners to them.
+ */
+
 function quizDifficultyPrompt() {
-    
+    const difficultyPrompt = document.getElementById("quiz-difficulty-prompt");
+    difficultyPrompt.classList.remove("hide");
+    heading.classList.add("hide");
+    menu.classList.add("hide");
+    const difficultyOptions = document.getElementsByClassName("difficulty-option");
+    for (let difficultyOption of difficultyOptions) {
+        difficultyOption.addEventListener("click", function() {
+            if (this.getAttribute("id") === "easy" ) {
+                console.log("easy quiz");
+            } else if (this.getAttribute("id") === "medium") {
+                console.log("medium quiz");
+            } else if (this.getAttribute("id") === "hard") {
+                console.log("hard quiz");
+            } 
+        })
+    }
 }

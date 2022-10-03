e451be214a6f741a6fce32dd651a98d7d0b3ad9f
Tony118g <guimaraesantony@gmail.com>
2022-10-03 10:27:15 +0000

    Add object to store easy questions in new javascript file

---
+++ assets/js/questions.js
@@ -0,0 +1,279 @@
+/*
+ideas on how to implement questions and answers structure 
+were taken from the following youtube tutorial:
+https://www.youtube.com/watch?v=riDzcEQbX6k 
+*/
+
+const easyQuestions = [
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    {
+        question: "easy question",
+        answers: [
+            { text: "answer 1", correct: false },
+            { text: "answer 2", correct: true },
+            { text: "answer 3", correct: false },
+            { text: "answer 4", correct: false }
+        ]
+    },
+    
+];
